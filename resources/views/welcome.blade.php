ab02185c51726bf2bf9eb693cc8e9a1339305bb2
Jamie Design BY JR <one4allplugin@gmail.com>
2023-09-18 13:45:14 +0100

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href={{asset("assets/img/favicon.ico")}}>
+    <title>Mitchell Mcdermott Blog</title>
+    <!-- Bootstrap core CSS -->
+    <link href={{asset("assets/css/bootstrap.min.css")}} rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+    <!-- Custom styles for this template -->
+
+    <link href={{asset("assets/css/mediumish.css")}} rel="stylesheet">
+</head>
+<body content="l">
+<!-- Begin Nav
+================================================== -->
+<nav class="navbar navbar-toggleable-md navbar-light bg-white fixed-top mediumnavigation">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="container">
+        <!-- Begin Logo -->
+        <a class="navbar-brand" href="index.html">
+            <img src={{asset("assets/logo.png")}} alt="logo">
+        </a>
+
+        <!-- End Logo -->
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <!-- Begin Menu -->
+            <ul class="navbar-nav ml-auto">
+
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Stories <span class="sr-only">(current)</span></a>
+                </li>
+                @if (Route::has('login'))
+                    @auth
+                        <li class="nav-item">
+                            <a href="{{ url('/dashboard') }}" class="nav-link">Dashboard</a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a href="{{ route('login') }}" class="nav-link">Log in</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a href="{{ route('register') }}" class="nav-link">Register</a>
+                            </li>
+                        @endif
+                    @endauth
+
+                @endif
+            </ul>
+            <!-- End Menu -->
+            <!-- Begin Search -->
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                <span class="search-icon"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M20.067 18.933l-4.157-4.157a6 6 0 1 0-.884.884l4.157 4.157a.624.624 0 1 0 .884-.884zM6.5 11c0-2.62 2.13-4.75 4.75-4.75S16 8.38 16 11s-2.13 4.75-4.75 4.75S6.5 13.62 6.5 11z"></path></svg></span>
+            </form>
+            <!-- End Search -->
+        </div>
+    </div>
+</nav>
+<!-- End Nav
+================================================== -->
+
+<!-- Begin Site Title
+================================================== -->
+<div class="container">
+    <div class="mainheading">
+        <h1 class="sitetitle">Mitchell Mcdermott Blog</h1>
+        <p class="lead">
+            A Tech and News Blog for the ages
+        </p>
+    </div>
+    <!-- End Site Title
+    ================================================== -->
+
+    <!-- Begin List Posts
+    ================================================== -->
+    <section class="recent-posts">
+        <div class="section-title">
+            <h2><span>All Stories</span></h2>
+        </div>
+        <div class="card-header col-12 mb-4">
+            <div class="push-10 col-2">
+                <select name="sort" id="sort" class="custom-select col-12">
+                    <option value="sort" disabled selected>Sort By</option>
+                    <option value="sort">Latest</option>
+                    <option value="sort">Oldest</option>
+                </select>
+            </div>
+        </div>
+        <div class="card-columns listrecent">
+
+       <!-- begin post -->
+            <div class="card">
+                <a href="post.html">
+                    <img class="img-fluid" src="{{asset("assets/demopic/5.jpg")}}" alt="">
+                </a>
+                <div class="card-block">
+                    <h2 class="card-title"><a href="post.html">Autumn doesn't have to be nostalgic, you know?</a></h2>
+                    <h4 class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</h4>
+                    <div class="metafooter">
+                        <div class="wrapfooter">
+						<span class="meta-footer-thumb">
+						<a href="author.html"><img class="author-thumb" src="https://www.gravatar.com/avatar/e56154546cf4be74e393c62d1ae9f9d4?s=250&amp;d=mm&amp;r=x" alt="Sal"></a>
+						</span>
+                            <span class="author-meta">
+						<span class="post-name"><a href="author.html">Sal</a></span><br/>
+						<span class="post-date">22 July 2017</span><span class="dot"></span><span class="post-read">6 min read</span>
+						</span>
+                            <span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd"></path></svg></a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end post -->
+
+            <!-- begin post -->
+            <div class="card">
+                <a href="post.html">
+                    <img class="img-fluid" src={{asset("assets/demopic/6.jpg")}} alt="">
+                </a>
+                <div class="card-block">
+                    <h2 class="card-title"><a href="post.html">Best galleries in the world with photos</a></h2>
+                    <h4 class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</h4>
+                    <div class="metafooter">
+                        <div class="wrapfooter">
+						<span class="meta-footer-thumb">
+						<a href="author.html"><img class="author-thumb" src="https://www.gravatar.com/avatar/e56154546cf4be74e393c62d1ae9f9d4?s=250&amp;d=mm&amp;r=x" alt="Sal"></a>
+						</span>
+                            <span class="author-meta">
+						<span class="post-name"><a href="author.html">Sal</a></span><br/>
+						<span class="post-date">22 July 2017</span><span class="dot"></span><span class="post-read">6 min read</span>
+						</span>
+                            <span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd"></path></svg></a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end post -->
+
+            <!-- begin post -->
+            <div class="card">
+                <a href="post.html">
+                    <img class="img-fluid" src={{asset("assets/demopic/7.jpg")}} alt="">
+                </a>
+                <div class="card-block">
+                    <h2 class="card-title"><a href="post.html">Little red dress and a perfect summer</a></h2>
+                    <h4 class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</h4>
+                    <div class="metafooter">
+                        <div class="wrapfooter">
+						<span class="meta-footer-thumb">
+						<a href="author.html"><img class="author-thumb" src="https://www.gravatar.com/avatar/e56154546cf4be74e393c62d1ae9f9d4?s=250&amp;d=mm&amp;r=x" alt="Sal"></a>
+						</span>
+                            <span class="author-meta">
+						<span class="post-name"><a href="author.html">Sal</a></span><br/>
+						<span class="post-date">22 July 2017</span><span class="dot"></span><span class="post-read">6 min read</span>
+						</span>
+                            <span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd"></path></svg></a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end post -->
+
+            <!-- begin post -->
+            <div class="card">
+                <a href="post.html">
+                    <img class="img-fluid" src="{{asset("assets/demopic/8.jpg")}}" alt="">
+                </a>
+                <div class="card-block">
+                    <h2 class="card-title"><a href="post.html">Thinking outside the box can help you prosper</a></h2>
+                    <h4 class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</h4>
+                    <div class="metafooter">
+                        <div class="wrapfooter">
+						<span class="meta-footer-thumb">
+						<a href="author.html"><img class="author-thumb" src="https://www.gravatar.com/avatar/e56154546cf4be74e393c62d1ae9f9d4?s=250&amp;d=mm&amp;r=x" alt="Sal"></a>
+						</span>
+                            <span class="author-meta">
+						<span class="post-name"><a href="author.html">Sal</a></span><br/>
+						<span class="post-date">22 July 2017</span><span class="dot"></span><span class="post-read">6 min read</span>
+						</span>
+                            <span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd"></path></svg></a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end post -->
+
+            <!-- begin post -->
+            <div class="card">
+                <a href="post.html">
+                    <img class="img-fluid" src="{{asset("assets/demopic/9.jpg")}}" alt="">
+                </a>
+                <div class="card-block">
+                    <h2 class="card-title"><a href="post.html">10 Things you should know about choosing your house</a></h2>
+                    <h4 class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</h4>
+                    <div class="metafooter">
+                        <div class="wrapfooter">
+						<span class="meta-footer-thumb">
+						<a href="author.html"><img class="author-thumb" src="https://www.gravatar.com/avatar/e56154546cf4be74e393c62d1ae9f9d4?s=250&amp;d=mm&amp;r=x" alt="Sal"></a>
+						</span>
+                            <span class="author-meta">
+						<span class="post-name"><a href="author.html">Sal</a></span><br/>
+						<span class="post-date">22 July 2017</span><span class="dot"></span><span class="post-read">6 min read</span>
+						</span>
+                            <span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd"></path></svg></a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end post -->
+
+            <!-- begin post -->
+            <div class="card">
+                <a href="post.html">
+                    <img class="img-fluid" src="{{asset("assets/demopic/10.jpg")}}" alt="">
+                </a>
+                <div class="card-block">
+                    <h2 class="card-title"><a href="post.html">Visiting the world means learning cultures</a></h2>
+                    <h4 class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</h4>
+                    <div class="metafooter">
+                        <div class="wrapfooter">
+						<span class="meta-footer-thumb">
+						<a href="author.html"><img class="author-thumb" src="https://www.gravatar.com/avatar/e56154546cf4be74e393c62d1ae9f9d4?s=250&amp;d=mm&amp;r=x" alt="Sal"></a>
+						</span>
+                            <span class="author-meta">
+						<span class="post-name"><a href="author.html">Sal</a></span><br/>
+						<span class="post-date">22 July 2017</span><span class="dot"></span><span class="post-read">6 min read</span>
+						</span>
+                            <span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd"></path></svg></a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end post -->
+
+        </div>
+    </section>
+    <!-- End List Posts
+    ================================================== -->
+
+
+</div>
+<!-- /.container -->
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src={{asset("assets/js/jquery.min.js")}}></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src={{asset("assets/js/bootstrap.js")}}></script>
+<script src={{asset("assets/js/ie10-viewport-bug-workaround.js")}}></script>
+</body>
+{{--
+        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+            @if (Route::has('login'))
+                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+        </div>
+--}}
+
